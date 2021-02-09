2b3a848bb12a669abcef3f137c98854d44ac54b5
Paul Zaremba <pzaremba@vsb.bc.ca>
2021-02-09 10:15:37 -0800

    example

---
+++ LessonOneB/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args){
+        //Variables
+        //Always have a good name.  This is a basic concept of data
+        //abstraction
+        int numberHotdogs;
+        double hotdogPrice = 2.5;
+        numberHotdogs = 5;
+        double taxRate = 1.12;
+        double totalPrice;
+        totalPrice = numberHotdogs * hotdogPrice * taxRate;
+        System.out.println("You owe: $" + totalPrice );
+        //Reference Data
+        String name = "Paul Zaremba";
+        int nameIndex = name.indexOf(" ");
+        String firstName = name.substring(0,nameIndex);
+        String lastname = name.substring(nameIndex + 1);
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastname);
+
+    }
+}
